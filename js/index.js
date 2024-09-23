edc6da7c81210f56a5c430db6b7e3f6565aca5ca
ArafatSani10 <mdarafatrahmansani17@gmail.com>
2024-09-23 22:13:37 +0600

    donate for quota Movement done,using java script.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-document.getElementById('btn-donate').addEventListener('click', function(event){
+document.getElementById('btn-donate').addEventListener('click', function (event) {
     event.preventDefault();
     let donateInput = parseFloat(document.getElementById('input-donation').value);
 
-    if(isNaN(donateInput)){
+    if (isNaN(donateInput)) {
         alert('please entered the valid Number');
     }
 
-    else if(donateInput < 0){
+    else if (donateInput < 0) {
         alert('Negetive Number are not allowed')
     }
 
-    else{
+    else {
         alert('Number is entered')
     }
     let coinNumber = parseFloat(document.getElementById('btn-add-coin').innerText);
@@ -24,16 +24,16 @@ document.getElementById('btn-donate').addEventListener('click', function(event){
 })
 
 // fenir calculation
-document.getElementById('btn-donate-feni').addEventListener('click', function(event){
+document.getElementById('btn-donate-feni').addEventListener('click', function (event) {
     event.preventDefault();
     let donateInputFeni = parseFloat(document.getElementById('donate-input-feni').value);
-    if(isNaN (donateInputFeni)){
+    if (isNaN(donateInputFeni)) {
         alert('please entered the valid Number');
     }
-    else if(donateInputFeni < 0){
+    else if (donateInputFeni < 0) {
         alert('Negetive Number are not allowed');
     }
-    else{
+    else {
         alert('Number is entered')
     }
 
@@ -45,4 +45,29 @@ document.getElementById('btn-donate-feni').addEventListener('click', function(ev
     let myCreditFeni = parseFloat(document.getElementById('my-cedit').innerText);
     let creditDonateFeni = myCreditFeni - donateInputFeni;
     document.getElementById('my-cedit').innerText = creditDonateFeni;
+})
+
+// quotar calculation
+document.getElementById('btn-donate-quota').addEventListener('click', function (event) {
+    event.preventDefault();
+    let donateInputQuota = parseFloat(document.getElementById('donate-input-quota').value);
+
+    if (isNaN(donateInputQuota)) {
+        alert('please entered the valid Number');
+    }
+    else if (donateInputQuota < 0) {
+        alert('Negetive Number are not allowed');
+    }
+    else {
+        alert('Number is entered')
+    }
+    let coinNumberQuota = parseFloat(document.getElementById('btn-add-coin-quota').innerText);
+    let totalDonateQuota = coinNumberQuota + donateInputQuota;
+    document.getElementById('btn-add-coin-quota').innerText = totalDonateQuota;
+
+    let myCreditQuota = parseFloat(document.getElementById('my-cedit').innerText );
+
+    let creditDonateQuota = myCreditQuota - donateInputQuota;
+    document.getElementById('my-cedit').innerText  = creditDonateQuota;
+
 })
