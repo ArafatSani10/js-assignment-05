8cbd11cb85bc3c71ee0e7446c8312b3dda18eea4
ArafatSani10 <mdarafatrahmansani17@gmail.com>
2024-09-23 21:58:00 +0600

    donate for feni calculation done using java script.

--- js/index.js
+++ js/index.js
@@ -23,3 +23,26 @@ document.getElementById('btn-donate').addEventListener('click', function(event){
     document.getElementById('my-cedit').innerText = creditDonate;
 })
 
+// fenir calculation
+document.getElementById('btn-donate-feni').addEventListener('click', function(event){
+    event.preventDefault();
+    let donateInputFeni = parseFloat(document.getElementById('donate-input-feni').value);
+    if(isNaN (donateInputFeni)){
+        alert('please entered the valid Number');
+    }
+    else if(donateInputFeni < 0){
+        alert('Negetive Number are not allowed');
+    }
+    else{
+        alert('Number is entered')
+    }
+
+    let coinNumberFeni = parseFloat(document.getElementById('btn-add-coin-feni').innerText);
+    let totalDonateFeni = coinNumberFeni + donateInputFeni;
+    document.getElementById('btn-add-coin-feni').innerText = totalDonateFeni;
+
+
+    let myCreditFeni = parseFloat(document.getElementById('my-cedit').innerText);
+    let creditDonateFeni = myCreditFeni - donateInputFeni;
+    document.getElementById('my-cedit').innerText = creditDonateFeni;
+})
